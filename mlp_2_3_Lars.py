5735b18a71678bb1628787bf451fe960e3207de1
LarsDaenen1 <90055095+LarsDaenen1@users.noreply.github.com>
2022-02-15 16:44:12 +0100

    Add files via upload

---
+++ mlp_2_3_Lars.py
@@ -0,0 +1,114 @@
+"""
+TU/e BME Project Imaging 2021
+Simple multiLayer perceptron code for MNIST
+Author: Suzanne Wetstein
+"""
+
+# disable overly verbose tensorflow logging
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+import tensorflow as tf
+
+
+# import required packages
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+
+# load the dataset using the builtin Keras method
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+y_train_copy = y_train.copy()
+y_test_copy = y_test.copy()
+
+y_train = np.ndarray(len(y_train_copy))
+y_test = np.ndarray(len(y_test_copy))
+
+for i in range(len(y_train_copy)):
+    if y_train_copy[i] in [1, 7]:
+        y_train[i] = 0
+    elif y_train_copy[i] in [0, 6, 8, 9]:
+        y_train[i] = 1
+    elif y_train_copy[i] in [2, 5]:
+        y_train[i] = 2
+    elif y_train_copy[i] in [3, 4]:
+        y_train[i] = 3
+    
+for i in range(len(y_test_copy)):
+    if y_test_copy[i] in [1, 7]:
+        y_test[i] = 0
+    elif y_test_copy[i] in [0, 6, 8, 9]:
+        y_test[i] = 1
+    elif y_test_copy[i] in [2, 5]:
+        y_test[i] = 2
+    elif y_test_copy[i] in [3, 4]:
+        y_test[i] = 3
+
+# derive a validation set from the training set
+# the original training set is split into 
+# new training set (90%) and a validation set (10%)
+X_train, X_val = train_test_split(X_train, test_size=0.10, random_state=101)
+y_train, y_val = train_test_split(y_train, test_size=0.10, random_state=101)
+
+
+
+# the shape of the data matrix is NxHxW, where
+# N is the number of images,
+# H and W are the height and width of the images
+# keras expect the data to have shape NxHxWxC, where
+# C is the channel dimension
+X_train = np.reshape(X_train, (-1,28,28,1)) 
+X_val = np.reshape(X_val, (-1,28,28,1))
+X_test = np.reshape(X_test, (-1,28,28,1))
+
+
+# convert the datatype to float32
+X_train = X_train.astype('float32')
+X_val = X_val.astype('float32')
+X_test = X_test.astype('float32')
+
+
+# normalize our data values to the range [0,1]
+X_train /= 255
+X_val /= 255
+X_test /= 255
+
+
+# convert 1D class arrays to 10D class matrices
+y_train = to_categorical(y_train, 4)
+y_val = to_categorical(y_val, 4)
+y_test = to_categorical(y_test, 4)
+
+
+model = Sequential()
+# flatten the 28x28x1 pixel input images to a row of pixels (a 1D-array)
+model.add(Flatten(input_shape=(28,28,1))) 
+# fully connected layer with 64 neurons and ReLU nonlinearity
+model.add(Dense(64, activation='relu'))
+# output layer with 10 nodes (one for each class) and softmax nonlinearity
+model.add(Dense(4, activation='softmax')) 
+
+
+# compile the model
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# use this variable to name your model
+model_name="my_first_model"
+
+# create a way to monitor our model in Tensorboard
+tensorboard = TensorBoard("logs/" + model_name)
+
+# train the model
+model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
+
+
+score = model.evaluate(X_test, y_test, verbose=0)
+
+
+print("Loss: ",score[0])
+print("Accuracy: ",score[1])
