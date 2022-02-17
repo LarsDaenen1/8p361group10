70028fecc080ace1faf05ee9ae15be03fed6df25
RobinvVelzen <94569671+RobinvVelzen@users.noreply.github.com>
2022-02-17 09:20:03 +0100

    Add files via upload

--- mlp.py
+++ mlp.py
@@ -60,31 +60,85 @@ y_train = to_categorical(y_train, 10)
 y_val = to_categorical(y_val, 10)
 y_test = to_categorical(y_test, 10)
 
+#%% Exersize 1
 
+for Layers in range(1,5):
+    for Nodes in [32,64,92,128]:
+        model = Sequential()
+        # flatten the 28x28x1 pixel input images to a row of pixels (a 1D-array)
+        model.add(Flatten(input_shape=(28,28,1))) 
+        # fully connected layer with 64 neurons and ReLU nonlinearity
+        for i in range(Layers):
+            model.add(Dense(Nodes, activation='relu'))
+        # output layer with 10 nodes (one for each class) and softmax nonlinearity
+        model.add(Dense(10, activation='softmax')) 
+
+
+        # compile the model
+        model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+        # use this variable to name your model
+        model_name="Model_" +str(Layers)+'_Layers_'+ str(Nodes)+ '_Nodes'
+    
+        # create a way to monitor our model in Tensorboard
+        tensorboard = TensorBoard("logs/" + model_name)
+        
+        # train the model
+        model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
+        
+        
+        score = model.evaluate(X_test, y_test, verbose=0)
+        
+        
+        print("Loss: ",score[0])
+        print("Accuracy: ",score[1])
+#%% Exersize 2
+#2a
 model = Sequential()
-# flatten the 28x28x1 pixel input images to a row of pixels (a 1D-array)
 model.add(Flatten(input_shape=(28,28,1))) 
-# fully connected layer with 64 neurons and ReLU nonlinearity
-model.add(Dense(64, activation='relu'))
-# output layer with 10 nodes (one for each class) and softmax nonlinearity
 model.add(Dense(10, activation='softmax')) 
-
-
-# compile the model
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-
-# use this variable to name your model
-model_name="my_first_model"
-
-# create a way to monitor our model in Tensorboard
+model_name="Model_2a"
 tensorboard = TensorBoard("logs/" + model_name)
-
-# train the model
 model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
-
-
+score = model.evaluate(X_test, y_test, verbose=0)
+#2b
+model = Sequential()
+model.add(Flatten(input_shape=(28,28,1))) 
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax')) 
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model_name="Model_2b"
+tensorboard = TensorBoard("logs/" + model_name)
+model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
+score = model.evaluate(X_test, y_test, verbose=0)
+#2c
+model = Sequential()
+model.add(Flatten(input_shape=(28,28,1))) 
+model.add(Dense(64, activation='linear')) # standard
+model.add(Dense(64, activation='linear'))
+model.add(Dense(64, activation='linear'))
+model.add(Dense(10, activation='softmax')) 
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model_name="Model_2c"
+tensorboard = TensorBoard("logs/" + model_name)
+model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
 score = model.evaluate(X_test, y_test, verbose=0)
 
-
-print("Loss: ",score[0])
-print("Accuracy: ",score[1])
+#%% Exersize 3
+y_train2= np.array([[i[1] or i[7], i[0] or i[6]or i[8] or i[9], i[2] or i[5], i[3] or i[4]] for i in y_train])
+y_val2 = np.array([[i[1] or i[7], i[0] or i[6]or i[8] or i[9], i[2] or i[5], i[3] or i[4]] for i in y_val])
+y_test2 = np.array([[i[1] or i[7], i[0] or i[6]or i[8] or i[9], i[2] or i[5], i[3] or i[4]] for i in y_test])
+model = Sequential()
+model.add(Flatten(input_shape=(28,28,1))) 
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(4, activation='softmax')) 
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model_name="Model_3"
+tensorboard = TensorBoard("logs/" + model_name)
+model.fit(X_train, y_train2, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val2), callbacks=[tensorboard])
+score = model.evaluate(X_test, y_test2, verbose=0)
